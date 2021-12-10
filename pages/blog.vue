66b9889eeaf05209561f7677c6cfa553a3f5e4e0
Amiran Gorgazjan <amiran.gorgazjan@tidal.com>
2021-12-10 22:54:27 +0100

    Init

---
+++ pages/blog.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="blog-container">
+    <div class="blog-header">
+      <h1>Blog</h1>
+    </div>
+    <div class="blog-content">
+      <div class="blog-post">
+        <h2>{{ post.title }}</h2>
+        <p>{{ post.content }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      post: {
+        title: 'My first blog post',
+        content: 'This is my first blog post. I hope you like it.'
+      }
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <nav>
+          <nuxt-link to="/blog">
+            My Blog
+          </nuxt-link>
+        </nav>
+
+        <h1 class="main-title">
+          Hello World
+        </h1>
+        <p>This is a simple example of a Vue.js component. <span class="my-message">{{ message }}</span></p>
+        <p>
+          <button class="btn btn-primary" @click="changeMessage">
+            Change Message
+          </button>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      message: 'Hello Vue!'
+    }
+  },
+  methods: {
+    changeMessage () {
+      this.message = 'Hello Silver!'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.container {
+  margin-top: 80px;
+}
+.my-message {
+  color: red;
+}
+</style>
